3f8c1f1cc81347595b6534d7287389df94fe4b81
GHANTA SAI BABU <147091454+GhantaSaiBabu@users.noreply.github.com>
2026-04-27 14:34:58 +0530

    Update geminiService.js

--- src/geminiService.js
+++ src/geminiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const GEMINI_API_KEY = "AIzaSyCf5NMgZouVH8U9SQdIevtCXC3D78W9VrI"; 
+const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`;
 
 export const getLiveRiskAnalysis = async (route, weather) => {
